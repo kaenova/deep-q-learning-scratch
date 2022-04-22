ff34d295699a774600baf1d12a957692b8400393
Kaenova Mahendra Auditama <kaenova@gmail.com>
2022-04-22 14:21:54 +0700

    Finalizing repo

---
+++ README.md
@@ -0,0 +1,11 @@
+# deep-q-learning-scratch
+
+Hello guys! welcome to this repository. This repository is for me to learn hot to translates from a research paper into an actual codes. The research i was trying to implement named [Playing Atari with Deep Reinforcement Learning](https://arxiv.org/abs/1312.5602).  
+
+You can see the code i wrote for this research paper in the notebooks `DeepQ Learning Algorithm`. For the game itself i handcrafted it manually. The game is pretty simple, the main goal is to make a player (number 1) moving towards into a target (number 2). You can play the game itself by running the python on `own_game.py` file.  
+
+To see the agent is playing correctly, you can check the notebooks and go to the `Playing` section. On that notebook, the agent can move correctly and going towards the target.  
+
+I analyze the model by running a tensorboard while on training phase. You can check the history and the logs by running tensorboard command `tensorboard --logdir ./tensorboard`.  
+
+Thanks for checking it out!
--- own_game.py
+++ own_game.py
@@ -12,52 +12,66 @@ class Game:
     num_actions = 2
     
     # State
-    # 0 = player position
-    # 1 = target position
+    # 0 = player position (1 on state space)
+    # 1 = target position (2 on state space)
     state = {
         0 : None,
         1 : None
     }
     
+    MAX_MOVEMENT = 5
+    current_movement = 5
+    
+    bot_idx = 0
+    up_idx = 9
+    
     def __init__(self) -> None:
-        player = random.randint(0, 5)
+        player = random.randint(self.bot_idx, self.up_idx)
         target = player
         while target == player:
-            target = random.randint(0,5)
+            x = random.randint(- self.MAX_MOVEMENT, self.MAX_MOVEMENT)
+            target = (x + player if x + player >= self.bot_idx and x + player <= self.up_idx else player)
         self.state = [player, target]
     
     def print_state(self):
-        print(f"Target Position : {self.state[1]}")
-        print(f"Player Position : {self.state[0]}")
-    
+        print(self.get_state())
+        
     def get_state(self):
-        # Must be converted to list or np.array to be processed into TF Models
-        return [self.state[0], self.state[1]]
+        state = [0 for _ in range(10)]
+        state[self.state[0]] = 1 # player assign
+        state[self.state[1]] = 2 # target assign
+        return state
     
     def execute_action(self, action_num):
         rewards = None
         
-        state_before = self.state
+        state_before = self.state.copy()
         if action_num == 0:
-            self.state[0] -= 1
+            if self.state[0] - 1 >= self.bot_idx:
+                self.state[0] -= 1
         else:
-            self.state[0] += 1
+            if self.state[0] + 1 <= self.up_idx:
+                self.state[0] += 1
             
         rewards = self.__calculate_rewards__(self.state, state_before)
+        
+        self.current_movement -= 1
         
         return self.get_state(), rewards, self.__check_game_end__()
     
     def __calculate_rewards__(self, current_state, previous_state):
         # Calculate the distance between player and target and normalize it
-        delta_current = (current_state[0] - current_state[1]) ** 2
-        delta_before = (previous_state[0] - previous_state[1]) ** 2
-        
-        if delta_current < delta_before:
+        current = abs(current_state[0] - current_state[1])
+        before = abs(previous_state[0] - previous_state[1])
+        if current < before:
             return 1
         else:
             return -1
     
     def __check_game_end__(self):
+        if self.current_movement == 0:
+            return 1
+        
         if self.state[0] == self.state[1]:
             return 1
         else:
